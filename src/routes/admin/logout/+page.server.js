ae94ee1a09bd125a28c8f42570352b17178bfe03
Nedit <nedxha19@htl-shkoder.com>
2025-09-03 20:15:32 +0200

    Nediti

--- src/routes/admin/logout/+page.server.js
+++
@@ -1,9 +0,0 @@
-import { logoutByToken } from '$lib/db/auth';
-import { redirect } from '@sveltejs/kit';
-
-export async function GET({ cookies }) {
-  const token = cookies.get('session');
-  if (token) await logoutByToken(token);
-  cookies.delete('session', { path: '/' });
-  throw redirect(302, '/');
-}
